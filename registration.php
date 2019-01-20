33089cd5df368599863318d22e0eb929d7415f63
Manuel Canepa <manuelcanepa@gmail.com>
2019-01-20 14:01:26 -0300

    Registro del modulo y composer

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * AFIP
+ *
+ * @category   Mugar
+ * @package    Mugar_Afip
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author     Mugar (https://www.mugar.io/)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mugar_Afip',
+    __DIR__
+);
